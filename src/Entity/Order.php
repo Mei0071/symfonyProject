c93380d10c4f3a84449fc1417d19fd45f317b74e
Mei0071 <mei-yi.ho@orange.fr>
2025-10-24 16:43:32 +0200

    Début : fichier entités

---
+++ src/Entity/Order.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\StatusOrder;
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $reference = null;
+
+    #[ORM\Column]
+    private ?\DateTime $createAt = null;
+
+    #[ORM\Column(enumType: StatusOrder::class)]
+    private ?StatusOrder $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): static
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTime
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTime $createAt): static
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?StatusOrder
+    {
+        return $this->status;
+    }
+
+    public function setStatus(StatusOrder $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
